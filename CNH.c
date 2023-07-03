3eeac700e0b2de41b27e9a19d5d8a8e80a4b6cad
Murilo <125324603+DRCO23@users.noreply.github.com>
2023-07-03 04:17:57 -0300

    Add CNH.c

---
+++ CNH.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+int idade;
+
+printf("Insira sua idade: ");
+scanf("%d", &idade);
+
+
+    if(idade>=18){
+        printf("Sua idade é %d \n",idade);
+        printf("Você está apto para solicitar sua CNH!");
+        }
+    else{
+        printf("Sua idade é %d \n",idade);
+        printf("Você não está apto para solicitar sua CNH!");
+        }
+
+return 0;
+
+}
